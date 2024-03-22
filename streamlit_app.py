02a1a5dbad7ef9e80bfde0a48212f8c570e29bd6
Anshsingh512 <141158714+Anshsingh512@users.noreply.github.com>
2024-03-22 22:34:03 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-import scikit-learn 
+import sklearn
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 
